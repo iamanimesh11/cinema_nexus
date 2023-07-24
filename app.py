aeb89a5e285e2a9535b56baf15466a0ba5730eba
iamanimesh11 <122896955+iamanimesh11@users.noreply.github.com>
2023-07-24 17:27:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,24 +36,30 @@ def recommend(movie):
     return recommended_movies_list,recommended_movies_list_poster
 
 st.sidebar.title("About this app")
-st.sidebar.title("About this app")
 st.sidebar.write("With a vast database of movies and a powerful recommendation algorithm, Movie Suggest helps you discover your next favorite films effortlessly.")
 st.sidebar.write("app generates personalized movie recommendations based on your favorite films. Simply type in the name of a movie you love, and Movie Suggest will present you with a handpicked list of similar movies that are sure to pique your interest.")
 st.sidebar.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--Animesh")
 
 st.title("Cinema Nexus..")
+st.markdown("Made by Animesh | [Github](https://github.com/iamanimesh11) | [LinkedIn](https://www.linkedin.com/in/animesh-singh11)")
+
 movie_names_options = [''] + movies_list['title'].values.tolist()
 movie_name_selections= st.selectbox('Type the movie name ',movie_names_options)
+st.text("Try movies : Avatar, Spectre ,Tangled,Interstelllar, Man of steel")
+
 #movie_name_selections= st.selectbox('Type the movie name ',movies_list['title'].values)
 
 if st.button("tell me more like this..."):
-    recommended_movie_names,recommended_movie_posters= recommend(movie_name_selections)
-    num_recommendations = len(recommended_movie_names)
-    cols = st.columns(num_recommendations)
-    for i, col in enumerate(cols):
-        with col:
-            st.text(recommended_movie_names[i])
-            st.image(recommended_movie_posters[i])
+     if movie_name_selections.strip() == "":
+        st.warning('Hey there is no movie without name huh.')
+    else:
+        recommended_movie_names,recommended_movie_posters= recommend(movie_name_selections)
+        num_recommendations = len(recommended_movie_names)
+        cols = st.columns(num_recommendations)
+        for i, col in enumerate(cols):
+            with col:
+                st.text(recommended_movie_names[i])
+                st.image(recommended_movie_posters[i])
 
 
 background_image = 'pngwing.com.png'  # Replace with the actual image file name
