3ba7417193da981f05b8e2d9f3678f26dec269dc
iamanimesh11 <122896955+iamanimesh11@users.noreply.github.com>
2023-07-24 17:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ st.text("Try movies : Avatar, Spectre ,Tangled,Interstelllar, Man of steel")
 
 if st.button("tell me more like this..."):
      if movie_name_selections.strip() == "":
-        st.warning('Hey there is no movie without name huh.')
+        st.warning('Movies await!🍿 Select one now')
      else:
         recommended_movie_names,recommended_movie_posters= recommend(movie_name_selections)
         num_recommendations = len(recommended_movie_names)
